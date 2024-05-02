fd96e931a06cc2ad000dd1121aa2415491ef3681
Patryk Bajak <Patryk.Bajak@microsoft.wsei.edu.pl>
2024-05-02 14:26:48 +0200

    installed necessary packages and initialized project structure

---
+++ Config/SportEventAppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SportEventAppApi.Config
+{
+    public class SportEventAppDbContext : DbContext
+    {
+        public SportEventAppDbContext(DbContextOptions<SportEventAppDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
